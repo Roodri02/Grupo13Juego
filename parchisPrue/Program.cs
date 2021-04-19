1f54badf787701999eeb2a8ca908771cc3e85e9d
Rodrigo Cabello <rodricab@ucm.es>
2021-04-19 12:18:19 +0200

    Añadidos módulos para customizar el color

--- parchisPrue/Program.cs
+++ parchisPrue/Program.cs
@@ -6,7 +6,8 @@ namespace parchisPrue
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("Cambio de colores");
         }
     }
 }
